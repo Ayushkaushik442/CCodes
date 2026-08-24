4ffd6758c3ff6f3952830034b54990f6965ebc89
Ayush Kaushik <kaushikayush442@gmail.com>
2026-08-24 22:05:57 +0530

    Add files via upload

---
+++ code2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int i,pos,n,arr[100];
+    printf("Enter the size  of array: ");
+    scanf("%d",&n);
+    printf("Enter the elements of the array ");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the element to search: ");
+    scanf("%d",&pos);
+    for(i=0;i<n;i++){
+        if(arr[i]==pos){
+            printf("Elememnts found at position %d",i+1);
+            pos=i+1;
+            break;
+        }
+        }
+    }
