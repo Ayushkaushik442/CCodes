414125f01639af083ec36f9bc457f5b93883a006
Ayush Kaushik <kaushikayush442@gmail.com>
2026-09-22 22:30:17 +0530

    2D matrix subtraction

--- code5.c
+++ code5.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int a[10][10],b[10][10],sum[10][10],i,j,r,c;
+    int a[10][10],b[10][10],subs[10][10],i,j,r,c;
     printf("Enter the number of rows and column of matrix: ");
     scanf("%d %d",&r,&c);
     printf("Enter the elements of first matrix: ");
@@ -15,11 +15,11 @@ int main(){
             scanf("%d",&b[10][10]);
         }
     }
-    printf("The sum of the two matrix are: ");
+    printf("The difference of the two matrix are: ");
     for(i=0;i<r;i++){
         for(j=0;j<c;j++){
-            sum[i][j]=a[i][j]+b[i][j];
-            printf("%d ",sum[i][j]);
+            subs[i][j]=a[i][j]-b[i][j];
+            printf("%d ",subs[i][j]);
         }
         printf("\n");
     }
